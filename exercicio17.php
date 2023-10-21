dbb0b50b3a3bf4ab0801f73f06782354dc4f9e23
Andreysson-Araujo <andreyssonaraujo@gmail.com>
2023-10-20 20:44:26 -0500

    16 a 18

---
+++ exercicio17.php
@@ -0,0 +1,19 @@
+<?php
+    if(15 > 5 AND "João" === "João") {
+        echo "True <br>";
+    }else{
+        echo "False<br>";
+    }
+
+    if ("Test" > 5 And 1) {
+        echo "True <br>";
+    }else{
+        echo "False<br>";
+    }
+    
+    if (2 == 3 and 5 >= 3) {
+        echo "True";
+    }else{
+        echo "False<br>";
+    }
+?>
